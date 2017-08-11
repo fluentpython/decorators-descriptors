41011992edadccb82d91d43d151555c13af61156
Luciano Ramalho <luciano@ramalho.org>
2017-08-11 12:48:22 -0700

    lineitem examples

---
+++ decorator/exercises/lab-descriptor/lineitem2.py
@@ -0,0 +1,47 @@
+"""
+==============
+LineItem tests
+==============
+
+A line item for a bulk food order has description, weight and price fields::
+
+    >>> raisins = LineItem('Golden raisins', 10, 6.95)
+    >>> raisins.quantity, raisins.product, raisins.price
+    (10, 'Golden raisins', 6.95)
+
+A ``subtotal`` method gives the total price for that line item::
+
+    >>> raisins.subtotal()
+    69.5
+
+The weight of a ``LineItem`` must be greater than 0::
+
+    >>> raisins.weight = -20
+    Traceback (most recent call last):
+        ...
+    ValueError: value must be > 0
+
+No change was made::
+
+    >>> raisins.weight
+    10
+
+Negative or 0 price is not acceptable either::
+
+    >>> truffle = LineItem('White truffle', 100, 0)
+    Traceback (most recent call last):
+        ...
+    ValueError: value must be > 0
+
+"""
+
+
+class LineItem:
+
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+
+    def subtotal(self):
+        return self.price * self.quantity
